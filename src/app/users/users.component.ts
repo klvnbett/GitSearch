ef5c349bc8448cfb50b55abd7bfea38e9652181f
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-25 14:12:42 +0300

    added the constructor for search name in the service

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,8 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { Repos } from '../models/repos';
 import { User } from '../models/user';
+import { UserserviceService } from '../userservice.service';
 
 @Component({
   selector: 'app-users',
@@ -11,7 +13,18 @@ export class UsersComponent implements OnInit {
   user!:User;
   repo!:Repos;
 
-  constructor() { }
+  constructor(public myService:UserserviceService,private repoService:UserserviceService) { }
+  searchs(searchName){
+    this.myService.searchUser(searchName).then(
+      (success)=>{
+        this.user=this.myService.foundUser;
+      },
+      (error)=>{
+        console.log(error)
+      }
+      
+    );
+  }
 
   ngOnInit(): void {
   }
