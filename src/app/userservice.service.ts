055eaf2ed4f3be38df32ae21e022758befe2270f
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-25 13:43:27 +0300

    added the user and repo imports

--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { User } from './models/user';
+import { Repos } from './models/repos';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
