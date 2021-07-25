904b277a4955f81828d64a316dd83fd10444a2a2
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-25 08:00:37 +0300

    declarations of variables i repos ts

--- src/app/models/repos.ts
+++ src/app/models/repos.ts
@@ -1,2 +1,4 @@
 export class Repos {
+    constructor(public name:string, public htmurl:string, public description:string, public createdOn:Date,
+        public forks:number,public count:number, public lang:string){}
 }
