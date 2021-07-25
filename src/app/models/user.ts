3068ef3f2035e56754cf5718ac3ad1ab34a9897a
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-25 07:55:39 +0300

    added user data declaration

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,8 @@
 export class User {
+    static output:any;
+    constructor(public url:string, public login:string, public htmurl:string,
+        public loc:string, public reposits:number, public followers:number, public following:number,public avataurl:string,
+        public createdOn:Date){
+
+        }
 }
