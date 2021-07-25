bfbe52e82032ffcc2ee39ae3f7d6475987cd849a
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-25 20:40:27 +0300

    updated the highlight color

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -11,7 +11,7 @@ export class HighlightDirective {
     this.highlight('blue');
   }
   @HostListener('mouseleave') onMouseLeave(){
-    this.highlight(null);
+    this.highlight("None");
   }
   private highlight(color:string){
     this.el.nativeElement.style.backgroundColor=color;
