7367a9c410544767e8e999145483c2a39b403467
Moringa School <kelvin.bett@student.moringaschool.com>
2021-07-25 08:24:22 +0300

    added path to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"user-profile",component:UsersComponent},
+  {path:"searchRepo",component:RepositoriesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
